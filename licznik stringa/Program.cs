2d1a453e84d05ef89a14c866b5208643ac5595e1
TheFatKidfromZamosc <witiuk61@gmail.com>
2025-01-09 09:02:59 +0100

    RELEASE THE SECOND KRAKEN

--- licznik stringa/Program.cs
+++ licznik stringa/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace licznik_stringa
+﻿// Program counts the lenghs of the string
+// Input : aaa
+// Output : 3
+namespace licznik_stringa
 {
     internal class Program
     {
@@ -7,7 +10,7 @@
             Console.WriteLine("Input a string :");
             string s = Console.ReadLine();
 
-            Console.WriteLine("The string lenght is :" + (s.Length+3));
+            Console.WriteLine("The string lenght is : " + (s.Length+3));
         }
     }
 }
