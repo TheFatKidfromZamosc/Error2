bae671c44d9412348b7b7565c73c2a7993e9b528
TheFatKidfromZamosc <witiuk61@gmail.com>
2025-01-09 09:01:02 +0100

    RELEASE THE KRAKEN

---
+++ licznik stringa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace licznik_stringa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string :");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("The string lenght is :" + (s.Length+3));
+        }
+    }
+}
